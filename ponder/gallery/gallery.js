1319b8dded07abcaa913ab27656306f0a2382527
ncfurtado <fur22004@byui.edu>
2026-05-14 10:43:51 -0600

    Finished the Gallery Ponder assignment and Restructured folder structure to match the cirriculum.

---
+++ ponder/gallery/gallery.js
@@ -0,0 +1,32 @@
+
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+// Event listener for opening the modal
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+    
+// Code to show modal  - Use event parameter 'e'   
+    console.log(e.target.src);
+    console.log(e.target.src.replace("sm", "full"));
+    modalImage.src = e.target.src.replace("sm", "full");
+    modal.showModal();
+}
+// Close modal on button click
+closeButton.addEventListener('click', () => {
+    modal.close();
+    modalImage.src = "";
+});
+
+// Close modal if clicking outside the image
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+        modalImage.src = "";
+
+    }
+});
+          
